27abbdfa0ce2f3d55704a44ebc91fc8258c5753f
Kaylien <kaylienphan@Kayliens-MacBook-Pro.local>
2017-11-19 15:58:38 -0500

    beginning to add websockets

--- streaming_rooms/lib/streaming_rooms_web/channels/user_socket.ex
+++ streaming_rooms/lib/streaming_rooms_web/channels/user_socket.ex
@@ -3,6 +3,7 @@ defmodule StreamingRoomsWeb.UserSocket do
 
   ## Channels
   # channel "room:*", StreamingRoomsWeb.RoomChannel
+  channel "updates:lobby", StreamingRoomsWeb.UpdatesChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
@@ -23,6 +24,15 @@ defmodule StreamingRoomsWeb.UserSocket do
     {:ok, socket}
   end
 
+  #def connect(%{"token" => token}, socket) do
+  #  # max_age: 1209600 is equivalent to two weeks in seconds
+  #  case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
+  #    {:ok, user_id} ->
+  #      {:ok, assign(socket, :user, user_id)}
+  #    {:error, reason} ->
+  #      :error
+  #  end
+  #end
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
   #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
